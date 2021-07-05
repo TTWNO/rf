b981e1f4154cb89805c7f154b691495b537db4a6
Tait Hoyem <tait@tait.tech>
2021-07-05 17:45:36 -0600

    Update code: add -d, -f options

--- rf.c
+++ rf.c
@@ -1,17 +1,26 @@
 #include <dirent.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <time.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <sys/stat.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #include <sys/types.h>
+
+#define FILE_ONLY_MASK 0x1
+#define DIRECTORY_ONLY_MASK 0x2
+#define RECURSIVE_MASK 0x4
+#define HIDDEN_FILE_MASK 0x8
+#define STDIN_MASK 0x10
+
+uint8_t options = 0x00;
 
 static int file_counter;
 static char** names;
 
-struct stat st;
+struct stat* st;
 
 void add_to_num_of_files(struct dirent* entry, int fi)
 {
@@ -23,16 +32,23 @@ void add_file_to_list(struct dirent* entry, int fi)
   strcpy(names[fi], entry->d_name);
 }
 
-
-int files_in_dir(DIR* dp, struct dirent* entry, bool hidden_files, void (*func)(struct dirent*, int)){
+/* TODO: this is not the prettiest thing I have ever written. Work on making it easier to maintain */
+int files_in_dir(DIR* dp, struct dirent* entry, void (*func)(struct dirent*, int)){
   int files = 0;
   while ((entry = readdir(dp))){
+    stat(entry->d_name, st);
     if (entry->d_name[0] != '.' ||
-        (hidden_files &&
+        (options & HIDDEN_FILE_MASK &&
         !strcmp(entry->d_name, "..") == 0 &&
         !strcmp(entry->d_name, ".") == 0)){
-      (*func)(entry, files);
-      ++files;
+      if ((options & FILE_ONLY_MASK &&
+          S_ISREG(st->st_mode)) ||
+          (options & DIRECTORY_ONLY_MASK &&
+          S_ISDIR(st->st_mode)) ||
+          (options ^ DIRECTORY_ONLY_MASK && options ^ FILE_ONLY_MASK)) {
+        (*func)(entry, files);
+        ++files;
+      }
     }
   }
   return files;
@@ -41,37 +57,71 @@ int files_in_dir(DIR* dp, struct dirent* entry, bool hidden_files, void (*func)(
 int main(int argc, char** argv){
   DIR* dp;
   struct dirent* entry;
-  bool hidden_files = false;
-
+  int opt, opts = 0;
+  int i;
+  st = malloc(sizeof(struct stat));
+  
   file_counter = 0;
 
   /* Best random thing I could come up with which is cross compileable 
    * Should be really random, even if it overflows :)*/
   srand(clock() * time(0));
-
-  int i = 0;
-  for (; i < argc; i++){
-    if (strcmp(argv[i], "-a") == 0 ||
-        strcmp(argv[i], "--all") == 0){
-      hidden_files = true;
+  while ((opt = getopt(argc, argv, "adfri")) != -1) {
+    switch (opt) {
+      case 'a':
+        options |= HIDDEN_FILE_MASK;
+        opts++;
+        break;
+      case 'd':
+        options |= DIRECTORY_ONLY_MASK;
+        opts++;
+        break;
+      case 'f':
+        options |= FILE_ONLY_MASK;
+        opts++;
+        break;
+      case 'r':
+        options |= RECURSIVE_MASK;
+        opts++;
+        break;
+      case 'i':
+        options |= STDIN_MASK;
+        opts++;
+        break;
+      default:
+        printf("Unsupported option \"-%c\".\n", opt);
+        exit(1);
     }
   }
-  if ((hidden_files && argc > 2) ||
-      (!hidden_files && argc > 1)){
-    dp = opendir(argv[argc-1]);
-  } else {
+
+  // stamp out invalid options
+  if (options & FILE_ONLY_MASK && options & DIRECTORY_ONLY_MASK) {
+    printf("-f and -d are mutually exclusive.\n");
+    exit(1);
+  }
+  if (options & STDIN_MASK && options != STDIN_MASK) {
+    printf("-i is not compatible with any other switch.\n");
+  }
+
+  if (strncmp("-", argv[argc-1], 1) == 0 || argc < 2) {
     dp = opendir(".");
+  } else {
+    dp = opendir(argv[argc-1]);
   }
 
   if (dp == NULL){
     perror("opendir");
-    return -1;
+    exit(1);
   }
 
-  files_in_dir(dp, entry, hidden_files, add_to_num_of_files); 
+  files_in_dir(dp, entry, add_to_num_of_files); 
   if (file_counter == 0){
-    fprintf(stderr, "directory is an empty directory.\n");
-    return -1;
+    if (options & FILE_ONLY_MASK || options & DIRECTORY_ONLY_MASK) {
+      fprintf(stderr, "no results.\n");
+    } else {
+      fprintf(stderr, "directory is an empty directory.\n");
+    }
+    exit(1);
   }
   rewinddir(dp);
   names = malloc(sizeof(char*) * file_counter);
@@ -79,7 +129,7 @@ int main(int argc, char** argv){
   {
     names[i] = malloc(sizeof(char) * 256);
   }
-  files_in_dir(dp, entry, hidden_files, add_file_to_list);
+  files_in_dir(dp, entry, add_file_to_list);
   int rando = rand()/((RAND_MAX + 1u)/file_counter);
   printf("%s\n", names[rando]);
 
